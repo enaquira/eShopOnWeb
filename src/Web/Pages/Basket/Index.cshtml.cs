35be793bbd5d18acfd05b8e800706c4938e5b300
John Doe <localy.dev@gmail.com>
2025-06-25 20:05:39 -0500

    bugfix/actualizacion-cantidad-producto

--- src/Web/Pages/Basket/Index.cshtml.cs
+++ src/Web/Pages/Basket/Index.cshtml.cs
@@ -59,7 +59,10 @@ public class IndexModel : PageModel
             return;
         }
 
-        throw new NotImplementedException();
+        var basketView = await _basketViewModelService.GetOrCreateBasketForUser(GetOrSetBasketCookieAndUserName());
+        var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
+        var basket = await _basketService.SetQuantities(basketView.Id, updateModel);
+        BasketModel = await _basketViewModelService.Map(basket);
     }
 
     private string GetOrSetBasketCookieAndUserName()
